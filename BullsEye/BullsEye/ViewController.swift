f28e8c03ab9865d0ec80870e31253a288ac3a463
Frics <jimmy9711@naver.com>
2020-07-28 17:39:07 +0900

    Swift UI를 이용한 화면 구성 예제 변수 선언 방법, 수직으로 object 배치(VStack) object -> Text, Button Button 터치하면 Pop Up 알람띄우기까지

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -14,18 +14,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
+     
     //Main.storyboard에 controller(button, label 등등 을 추가하고
-    //ViewController에 기능을 구현 -> 서로 connection을 해줌 
+    //ViewController에 기능을 구현 -> 서로 connection을 해줌
     @IBAction func showAlert(){
         print("hello!")
-        let alert = UIAlertController(title: "알람!", message: "이거슨 알람임", preferredStyle: .alert)
+        let alert = UIAlertController(title: "알람!", message: "이거슨 알람임", preferredStyle: .alert) //alert pop up 구현
         
-        let action = UIAlertAction(title: "확인!", style: .default, handler: nil)
+        let action = UIAlertAction(title: "확인!", style: .default, handler: nil) //alert pop up에 확인 버튼 구현
         
-        alert.addAction(action)
+        alert.addAction(action) //alert와 action을 이어줌
         
-        present(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil) //구현한 alert pop up을 동작하게 해줌
         
        
     }
---
+++ Bullseye_SwiftUI/Bullseye_SwiftUI/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  Bullseye_SwiftUI
+//
+//  Created by James Choi on 2020/07/28.
+//  Copyright © 2020 James Choi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    //alertIsVisible이라는 부울리언 변수 선언
+    //@State가 뭔지는 더 알아봐야함
+    @State var alertIsVisible: Bool = false
+    @State var KnockButton: Bool = false
+    
+    //body라는 이름의 변수,
+    //some View는 body라는 변수가 View라는것을 나타냄
+    //View가 아닐수도 있긴함
+    var body: some View {
+        //수직으로 object를 넣는게 VStack
+        VStack{
+            //텍스트 오브젝트
+            Text("My First iOS app!!!!")
+                .fontWeight(.bold)//얘도 메소드
+                .foregroundColor(Color.red) //얘도 메소드
+            //버튼 오브젝트
+            Button(action:{
+                //버튼이 눌렀음을 쉘에서 확인할 수 있게 해주는 print메소드
+                print("Button Pressed!")
+                print(self.alertIsVisible)
+                self.alertIsVisible = true
+            }){
+                Text("확인")
+            }
+                //alertIsVisible이 true일때 .alert가 출력
+                //내용은 아래처럼 추가
+                //버튼이 눌리고 나면 alertIsVislble 변수는 다시 false로 돌아가는듯?
+                .alert(isPresented: $alertIsVisible){ () ->
+                    Alert in
+                    return Alert(title: Text("안녕!"), message: Text("팝업 메세지입니당"), dismissButton: .default(Text("WOWOWOWOW!")))
+            }
+            Button(action:{
+                print("It is knock knock button")
+                self.KnockButton = true
+            }){
+                Text("Knock, Knock")
+            }
+            .alert(isPresented: $KnockButton){ () ->
+                Alert in
+                return Alert(title: Text("똒똒"), message: Text("Knock Knock 버튼입니다."), dismissButton: .default(Text("껄껄껄")))
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
